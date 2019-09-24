6c7e217a808c144d5ab2a46fefb18eda91761d9a
rcrowe-google <robertcrowe@google.com>
2019-09-24 15:20:57 -0700

    airflow and jupyter

--- tfx_airflow/host_start.sh
+++ tfx_airflow/host_start.sh
@@ -16,4 +16,8 @@ printf "${GREEN}Starting workshop container${NORMAL}\n\n"
 printf "${GREEN}Next Step:${NORMAL}\n"
 printf "${GREEN}When you see the prompt 'root@<hex id>:/#'${NORMAL}\n"
 printf "${GREEN}Enter 'source setup_demo.sh'${NORMAL}\n"
+printf "${GREEN}Wait for init to complete, and create a password'${NORMAL}\n"
+printf "${GREEN}Airflow and Jupyter will then both start:'${NORMAL}\n"
+printf "${GREEN}Airflow: Open a browser and go to http://localhost:8080'${NORMAL}\n"
+printf "${GREEN}Jupyter: Open a browser and go to http://localhost:8888'${NORMAL}\n"
 docker-compose -f ../docker-compose.yaml run --service-ports tfx
